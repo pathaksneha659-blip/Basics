c682438829f6bad126594ca599c5cac2691e85a0
pathaksneha659--bil <pathaksneha659@gmail.com>
2025-10-22 19:36:31 +0530

    case2

--- week.cpp
+++ week.cpp
@@ -5,8 +5,11 @@ int main(){
     cout<<"Enter the day:"<<endl;
     cin>>day;
     switch(day){
-        case '1':
+        case 1:
         cout<<"Monday";
+        case 2:
+        cout<<"Tuesday";
+
     }
 
 }
