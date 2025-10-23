71f96707c85d214f16995a58313f657471b81eee
pathaksneha659--bil <pathaksneha659@gmail.com>
2025-10-23 21:18:49 +0530

    new

---
+++ odd.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=100;i++){
+        if(i%2==0) continue;
+        cout<<"All odd numbers between them is:"<<endl;
+        cout<<i<<endl;
+    }
+}
